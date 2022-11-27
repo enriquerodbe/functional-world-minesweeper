fe5c40d93608178d928362bd55db2b484612aa06
Enrique Rodríguez <enrique.rodriguez@scalac.io>
2022-11-27 16:10:46 -0300

    Add BoardFactorySpec to force improve implementation

--- squared/src/main/scala/io/scalac/minesweeper/squared/SquaredBoard.scala
+++ squared/src/main/scala/io/scalac/minesweeper/squared/SquaredBoard.scala
@@ -6,10 +6,19 @@ class SquaredBoard extends Board:
   override def uncover(coordinate: Coordinate): Board =
     new SquaredBoard:
       override def stateAt(_coordinate: Coordinate): Coordinate.State =
-        Coordinate.State.Uncovered
+        if (SquaredBoard.this.stateAt(_coordinate) == Coordinate.State.Flagged)
+          Coordinate.State.Flagged
+        else
+          Coordinate.State.Uncovered
 
       override def state: Board.State =
-        Board.State.Won
+        if hasMine(coordinate) then
+          if stateAt(coordinate) == Coordinate.State.Flagged then
+            SquaredBoard.this.state
+          else
+            Board.State.Lost
+        else
+          Board.State.Won
 
   override def flag(coordinate: Coordinate): Board =
     new SquaredBoard:
@@ -17,10 +26,10 @@ class SquaredBoard extends Board:
         Coordinate.State.Flagged
 
   override def allCoordinates: Seq[Coordinate] =
-    Seq(SquaredCoordinate(0, 0))
+    Seq.tabulate(5)(SquaredCoordinate(_, 0))
 
   override def hasMine(coordinate: Coordinate): Boolean =
-    false
+    coordinate.hashCode() % 2 == 0
 
   override def stateAt(coordinate: Coordinate): Coordinate.State =
     Coordinate.State.Covered
