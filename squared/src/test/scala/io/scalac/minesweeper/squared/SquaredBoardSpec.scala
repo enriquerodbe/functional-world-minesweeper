f40e323949b544ef9015ff7b803fe366748924ba
Enrique Rodríguez <enrique.rodriguez@scalac.io>
2022-11-27 16:32:40 -0300

    Test squared implementation specifically

--- squared/src/test/scala/io/scalac/minesweeper/squared/SquaredBoardSpec.scala
+++ squared/src/test/scala/io/scalac/minesweeper/squared/SquaredBoardSpec.scala
@@ -2,4 +2,14 @@ package io.scalac.minesweeper.squared
 
 import io.scalac.minesweeper.api.BoardSpec
 
-class SquaredBoardSpec extends BoardSpec(SquaredBoardFactory())
+class SquaredBoardSpec extends BoardSpec(SquaredBoardFactory()):
+  test("Should contain all coordinates") {
+    val board = new SquaredBoard(9, _ => false)
+    val coordinates = for {
+      x <- 0 until 3
+      y <- 0 until 3
+    } yield SquaredCoordinate(x, y)
+
+    board.allCoordinates.foreach(c => assert(coordinates.contains(c)))
+    coordinates.foreach(c => assert(board.allCoordinates.contains(c)))
+  }
